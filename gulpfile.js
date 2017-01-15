dc12f09e794178969ae25b16ec2109254760f3a2
syuilo <syuilotan@yahoo.co.jp>
2017-01-15 09:03:08 +0900

    bye bye Babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const gulp = require('gulp');
-const babel = require('gulp-babel');
 const ts = require('gulp-typescript');
 const es = require('event-stream');
 
@@ -14,9 +13,6 @@ gulp.task('build', () => {
 
 	return es.merge(
 		tsResult.js
-			.pipe(babel({
-				presets: ['es2015', 'stage-3']
-			}))
 			.pipe(gulp.dest('./built/')),
 		tsResult.dts.pipe(gulp.dest('./built/'))
 	);
