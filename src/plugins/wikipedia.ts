f491e40784bab3677e36d97af1b0c301a8d43a0b
syuilo <syuilotan@yahoo.co.jp>
2017-01-30 08:48:16 +0900

    Add debug

--- src/plugins/wikipedia.ts
+++ src/plugins/wikipedia.ts
@@ -1,5 +1,8 @@
 import * as URL from 'url';
 import * as request from 'request';
+import * as debug from 'debug';
+
+const log = debug('summaly:plugins:wikipedia');
 
 exports.test = (url: URL.Url) =>
 	/\.wikipedia\.org$/.test(url.hostname);
@@ -9,7 +12,12 @@ exports.summary = (url: URL.Url) => new Promise((res, rej) => {
 	const title = url.pathname.split('/')[2];
 	const endpoint = `https://${lang}.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=${title}`;
 
+	log(`lang is ${lang}`);
+	log(`title is ${title}`);
+	log(`endpoint is ${endpoint}`);
+
 	request(endpoint, (err, _, body) => {
+		log(body);
 		body = JSON.parse(body);
 		const info = body.query.pages[Object.keys(body.query.pages)[0]];
 		res({
