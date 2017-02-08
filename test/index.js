ac5e6230f6a8a6b93269c8b9241b78b120e4099e
syuilo <syuilotan@yahoo.co.jp>
2017-02-08 21:33:37 +0900

    [Test] Add some twitter cards tests

--- test/index.js
+++ test/index.js
@@ -78,3 +78,44 @@ describe('OGP', () => {
 		});
 	});
 });
+
+describe('TwitterCard', () => {
+	it('title', done => {
+		const app = express();
+		app.use((req, res) => {
+			res.sendFile(__dirname + '/htmls/twitter-title.html');
+		});
+		const server = app.listen(port, async () => {
+			const summary = await summaly(host);
+			assert.equal(summary.title, 'Strawberry Pasta');
+			server.close();
+			done();
+		});
+	});
+
+	it('description', done => {
+		const app = express();
+		app.use((req, res) => {
+			res.sendFile(__dirname + '/htmls/twitter-description.html');
+		});
+		const server = app.listen(port, async () => {
+			const summary = await summaly(host);
+			assert.equal(summary.description, 'Strawberry Pasta');
+			server.close();
+			done();
+		});
+	});
+
+	it('thumbnail', done => {
+		const app = express();
+		app.use((req, res) => {
+			res.sendFile(__dirname + '/htmls/twitter-image.html');
+		});
+		const server = app.listen(port, async () => {
+			const summary = await summaly(host);
+			assert.equal(summary.thumbnail, 'https://himasaku.net/himasaku.png');
+			server.close();
+			done();
+		});
+	});
+});
