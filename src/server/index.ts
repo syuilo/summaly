285124e96b892bae080c980ec936268291917158
syuilo <syuilotan@yahoo.co.jp>
2018-08-29 15:03:35 +0900

    Add standalone server

---
+++ src/server/index.ts
@@ -0,0 +1,21 @@
+import * as http from 'http';
+import * as Koa from 'koa';
+import summaly from '../';
+
+const app = new Koa();
+
+app.use(async ctx => {
+	try {
+		const summary = await summaly(ctx.query.url, {
+			followRedirects: false
+		});
+
+		ctx.body = summary;
+	} catch (e) {
+		ctx.status = 500;
+	}
+});
+
+const server = http.createServer(app.callback());
+
+server.listen(80);
